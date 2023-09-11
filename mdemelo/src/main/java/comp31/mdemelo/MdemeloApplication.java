c2dd2b00cd8b719a1a027ac71907869a167be769
Marco <marco.demelo2@slc.student.on.ca>
2023-09-11 17:50:34 -0400

    Initial Commit

---
+++ mdemelo/src/main/java/comp31/mdemelo/MdemeloApplication.java
@@ -0,0 +1,13 @@
+package comp31.mdemelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdemeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdemeloApplication.class, args);
+	}
+
+}
---
+++ mdemelo/src/test/java/comp31/mdemelo/MdemeloApplicationTests.java
@@ -0,0 +1,13 @@
+package comp31.mdemelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdemeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
