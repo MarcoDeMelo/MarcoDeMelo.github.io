1191b9d7064f304bcca252d9e29a6e45f2e2bde9
Marco <marco.demelo2@slc.student.on.ca>
2023-09-13 16:25:01 -0400

    Added 3 templates and MainController handles names

--- mdemelo/src/main/java/comp31/mdemelo/MainController.java
+++ mdemelo/src/main/java/comp31/mdemelo/MainController.java
@@ -10,12 +10,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
     
     @GetMapping("/login")
-    public String getLogin(@RequestParam String userId, Model model)
-    {
+    public String getLogin(@RequestParam String userId, Model model) {
+        String targetPage;
+    
+        // Check the user's userId and determine the target page based on their name and job role
+        if ("Sam".equals(userId) || "Sue".equals(userId)) {
+            targetPage = "salesLogin";  // Redirect to sales login page for Sam and Sue
+        } else if ("Olivia".equals(userId) || "Ollie".equals(userId)) {
+            targetPage = "ordersLogin";  // Redirect to orders login page for Olivia and Ollie
+        } else if ("Rachel".equals(userId) || "Ralph".equals(userId)) {
+            targetPage = "repairsLogin";  // Redirect to repairs login page for Rachel and Ralph
+        } else {
+            targetPage = "home";  // Default to the "home" page if no specific name is matched
+        }
+        
+    
+        // Add the userId to the model, if needed for the specific login page
         model.addAttribute("userId", userId);
-        return"home";
+    
+        // Return the determined target page
+        return targetPage;
     }
-
+    
     @GetMapping("/image")
     public String image(Model model)
     {
