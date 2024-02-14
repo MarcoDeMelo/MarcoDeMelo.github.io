ce0fd8207fefbbc5adfe243c882aeae526547064
Marco <marco.demelo2@student.sl.on.ca>
2024-02-14 06:24:59 -0500

    For nick valentines

--- mdemelo/src/main/java/comp31/mdemelo/MainController.java
+++ mdemelo/src/main/java/comp31/mdemelo/MainController.java
@@ -3,40 +3,36 @@ package comp31.mdemelo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
 public class MainController {
     
-    @GetMapping("/login")
-    public String getLogin(@RequestParam String userId, Model model) {
-        String targetPage;
+  
+   
     
-        // Check the user's userId and determine the target page based on their name and job role
-        if ("Sam".equals(userId) || "Sue".equals(userId)) {
-            targetPage = "salesLogin";  // Redirect to sales login page for Sam and Sue
-        } else if ("Olivia".equals(userId) || "Ollie".equals(userId)) {
-            targetPage = "ordersLogin";  // Redirect to orders login page for Olivia and Ollie
-        } else if ("Rachel".equals(userId) || "Ralph".equals(userId)) {
-            targetPage = "repairsLogin";  // Redirect to repairs login page for Rachel and Ralph
-        } else {
-            targetPage = "home";  // Default to the "home" page if no specific name is matched
-        }
-        
-    
-        // Add the userId to the model, if needed for the specific login page
-        model.addAttribute("userId", userId);
-    
-        // Return the determined target page
-        return targetPage;
-    }
-    
-    @GetMapping("/image")
+    @GetMapping("/images")
     public String image(Model model)
     {
-        model.addAttribute("name", "Ellie");
+       
         return "imagePage";
+        
+    }
+
+    @GetMapping("/date")
+    public String date(Model model)
+    {
+       
+        return "date";
+        
+    }
+
+    
+    @GetMapping("/final")
+    public String getFinal(Model model)
+    {
+       
+        return "final";
         
     }
 }
